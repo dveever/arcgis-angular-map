760c79ce3906367fa636250652a0896765f2b6da
Dolbik-VorobeyIV <Dolbik-VorobeyIV@tmn.lukoil.com>
2020-02-21 11:23:04 +0500

    add are comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,31 @@ import {loadModules} from 'esri-loader';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Div for card
+  // Get a container link for map place
   @ViewChild('mapView', {static: true}) private readonly mapViewElement: ElementRef;
   // main map view
   private mapView;
   title = 'ArcGIS angular map';
 
   constructor() {
+    // This function to load Dojo's require the classes listed in the array modules
     loadModules(['esri/Map', 'esri/views/MapView'])
       .then(([Map, MapView]: [__esri.MapConstructor, __esri.MapViewConstructor]) => {
         // set default map properties
+        //
         const mapProperties = {
           basemap: 'gray'
         }
         // create map by default properties
         const map = new Map(mapProperties);
-
         // set default map view properties
         // container - element in html-template for locate map
-        // zoom - default zoom param
+        // zoom - default zoom parameter, value from 1 to 18
         const mapViewProperties = {
           container: this.mapViewElement.nativeElement,
           zoom: 3,
           map
         }
-
         // create map view by default properties
         this.mapView = new MapView(mapViewProperties);
       });

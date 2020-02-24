f4ecdf1a04062343611e99829877905c156cd762
gisview <Qvbn1232>
2020-02-24 21:08:24 +0500

    add a layer into map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,14 @@ export class AppComponent {
 
   constructor() {
     // This function to load Dojo's require the classes listed in the array modules
-    loadModules(['esri/Map', 'esri/views/MapView'])
-      .then(([Map, MapView]: [__esri.MapConstructor, __esri.MapViewConstructor]) => {
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/MapImageLayer'])
+      .then(([Map, MapView, MapImageLayer]: [__esri.MapConstructor, __esri.MapViewConstructor,
+        __esri.MapImageLayerConstructor]) => {
         // set default map properties
         //
         const mapProperties = {
           basemap: 'gray'
-        }
+        };
         // create map by default properties
         const map = new Map(mapProperties);
         // set default map view properties
@@ -31,9 +32,22 @@ export class AppComponent {
           container: this.mapViewElement.nativeElement,
           zoom: 3,
           map
-        }
+        };
         // create map view by default properties
         this.mapView = new MapView(mapViewProperties);
+
+        // Set service properties
+        // url - this address to MapServer from ArcGIS Enterprise
+        // sublayers - this are the settings for the inner layers of the service.
+        // id = 1 it tell us that will be displayed only one layer with the identifier
+        const oilSandLayerProperties = {
+          url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/OilSandsProjectBoundaries/MapServer',
+          sublayers: [{id: 1}]
+        };
+        // Create map image layer by properties
+        const oilSandsLayer = new MapImageLayer(oilSandLayerProperties);
+        // Adding a layer into map
+        map.add(oilSandsLayer);
       });
   }
 }
